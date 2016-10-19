ed841e8609be27328c06d14e1bc935a05e0724ff
Louis <ldicarro@mindspring.com>
2016-10-19 00:33:20 -0400

    -added comments -made pins consts

--- pumpkin.ino
+++ pumpkin.ino
@@ -1,19 +1,20 @@
-int ledPin = 13;
-int bzzPin = 12;
-int mtrPin = 11;
-int dbgPin = 10;
+// setting pins
+const int ledPin = 13;  // led
+const int bzzPin = 12;  // buzzer
+const int mtrPin = 11;  // motor
+const int dbgPin = 10;  // indicator that the PIR is high
+const int inputPin = 2; // input of PIR
 
-int inputPin = 2;
-int PIRstate = LOW;
-int animation = LOW;
-int val = 0;
-int countLength = 100;
-int count = 0;
+int PIRstate = LOW;    // state of sensor
+int animation = LOW;   // animation flag
+int val = 0;           // store the dig read of sensor
+int animLength = 100;  // how long the animation runs for
+int animCount = 0;     // counter
 
-int ledState = LOW;
-int ledInterval = 10;
-int ledCount = 0;
-int bzzState = LOW;
+int ledState = LOW;    // initial state of led pin
+int ledInterval = 10;  // interval of led (going to millis())
+int ledCount = 0;      // counter for led (also going to millis())
+int bzzState = LOW;    // etc...
 int bzzInterval = 15;
 int bzzCount = 0;
 int mtrState = LOW;
@@ -22,18 +23,20 @@ int mtrCount = 0;
 
 void setup()
 {
+  // set up pins
   pinMode(ledPin,OUTPUT);
   pinMode(bzzPin,OUTPUT);
   pinMode(mtrPin,OUTPUT);
   pinMode(dbgPin,OUTPUT);
   pinMode(inputPin,INPUT);
 
+  // so we can trace out our values
   Serial.begin(9600);
 }
 
 void loop()
 {
-  // read if PIR senses
+  // read if PIR senses something
   val = digitalRead(inputPin);
 
   if(val == HIGH)
@@ -52,26 +55,28 @@ void loop()
   // turning on the animation
   if(PIRstate == HIGH && animation == LOW)
   {
-    animation = HIGH;
-    count = countLength;
-    ledState = HIGH;
+    // this is the state where animation has not been running but PIR has sensed something
+    animation = HIGH;        // set animation flag to running
+    animCount = animLength;  // set the counter for the animation
+    ledState = HIGH;         // turn on the flag for the led, etc...
     bzzState = HIGH;
     mtrState = HIGH;
-    ledCount = ledInterval;
+    ledCount = ledInterval;  // set the counter for the led, etc...
     bzzCount = bzzInterval;
     mtrCount = mtrInterval;
   }
   else if(PIRstate == HIGH && animation == HIGH)
   {
-
+    // PIR sensed something and the animation timer is running
   }
   else if(PIRstate == LOW && animation == HIGH )
   {
-    
+    // PIR stopped sensing and animation timer is running
   }
   else if(PIRstate == LOW && animation == LOW)
   {
-    ledState = LOW;
+    // PIR stopped sensing and animation timer is done
+    ledState = LOW; // turn off the flag for the led, etc...
     bzzState = LOW;
     mtrState = LOW;
   }
@@ -83,15 +88,15 @@ void loop()
     doBzzAnimation();
     doMtrAnimation();
 
-    count--;
-    if(count == 0)
+    animCount--;  // subtract one from the animation counter
+    if(animCount == 0) // if the counter has run out
     {
-      animation = LOW;
+      animation = LOW; // set the animation flag to low
     }
   }
   else
   {
-    // set everything to LOW
+    // animation is over set everything to LOW
     animation = LOW;
     ledState = LOW;
     bzzState = LOW;
@@ -117,12 +122,13 @@ void loop()
 
 void doLEDAnimation()
 {
+  // if the led counter has run out
   if(ledCount == 0)
   {
-    ledState = !ledState;
-    ledCount = ledInterval;
+    ledState = !ledState;   // set the state of the led to its opposite
+    ledCount = ledInterval; // reset the counter
   }
-  ledCount--;
+  ledCount--; // decrement the counter
 }
 
 void doBzzAnimation()
